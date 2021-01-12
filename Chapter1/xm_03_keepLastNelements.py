688f2985ececcf17a1183236c71c7390ac388c69
xiaomengli <13155328119@163.com>
2021-01-12 23:04:47 +0800

    update keepLastNelemenys.py

--- Chapter1/xm_03_keepLastNelements.py
+++ Chapter1/xm_03_keepLastNelements.py
@@ -49,3 +49,26 @@ with open(r"./somefile") as f:
         if "python" in line:
             print("nb")
 
+"""
+在迭代操作或者其他操作的时候，怎样只保留最后有限几个元素的历史记录？
+保留有限历史记录正是 collections.deque 大显身手的时候。比如，下面的代码在多行上面做简单的文本匹配， 并返回匹配所在行的最后N行：
+"""
+
+
+def search(lines, pattern, history=2):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+
+with open(r"./somefile") as f:
+    for line, prevlines in search(f, 'java', 2):
+        print("line" + line, end='')
+
+print('-' * 40)
+
+with open(r"./somefile") as f:
+    for line, prevlines in search(f, 'java', 2):
+        print("preline", prevlines)
